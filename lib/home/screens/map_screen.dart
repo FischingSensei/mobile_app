64052a75a7cdc0885eb4e8061f64847072597cae
NotaSurgele <alexis.picard@epitech.eu>
2025-02-23 15:03:19 +0100

    feat: leaflet impl

--- lib/home/screens/map_screen.dart
+++ lib/home/screens/map_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class MapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("Hello world", style: TextStyle(color: Colors.red),),
+      child: FlutterMap(
+        children: <Widget>[
+          TileLayer( // Bring your own tiles
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+            userAgentPackageName: 'com.example.app', // Add your app identifier
+            // And many more recommended properties!
+          ),
+        ]
+      ),
     );
   }
 }
