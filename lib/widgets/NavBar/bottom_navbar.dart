b3c392410e121f239147dbc7f581729ca5b9590a
NotaSurgele <alexis.picard@epitech.eu>
2025-02-25 15:40:29 +0100

    feat(navbar) add animation on select button

--- lib/widgets/NavBar/bottom_navbar.dart
+++ lib/widgets/NavBar/bottom_navbar.dart
@@ -12,65 +12,63 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  List<bool> selected = [true, false, false];
+  List<(bool, double)> selected = [(true, 20), (false, 0), (false, 0)];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.sizeOf(context).height * 0.06,
-      child: Center(
-        child: Container(
-          width: MediaQuery.sizeOf(context).width * 0.6,
-          height: MediaQuery.sizeOf(context).height * 0.06,
-          decoration: navbarDecoration,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              for (int i = 0; i < 3; i++) ...[
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Force the divider to have a specific width
-                    if (selected[i]) ...[
-                      SizedBox(
-                        width: 20, // Adjust as needed
-                        height: 5,
-                        child: Divider(
-                          color: Colors.white,
-                          thickness: 2,
-                          // Remove or adjust 'height' if you want space above the icon
+        height: MediaQuery.sizeOf(context).height * 0.06,
+        child: Center(
+          child: Container(
+              width: MediaQuery.sizeOf(context).width * 0.6,
+              height: MediaQuery.sizeOf(context).height * 0.06,
+              decoration: navbarDecoration,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (int i = 0; i < 3; i++) ...[
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        AnimatedContainer(
+                          duration: Duration(milliseconds: 100),
+                          curve: Curves.easeInOut,
+                          width: selected[i].$2,
+                          child: SizedBox(
+                            height: 0,
+                            child: Divider(
+                              color: Colors.white,
+                              thickness: 2,
+                            ),
+                          ),
                         ),
-                      ),
-                    ],
-                    NavBarButton(
-                      onPressed: () {
-                        setState(() {
-                          selected = [false, false, false];
-                          selected[i] = true;
-                          widget.onClick(i);
-                        });
-                      },
-                      icon: const Icon(
-                        Icons.home,
-                        color: Colors.white,
-                        size: 25,
-                      ),
+                        NavBarButton(
+                          onPressed: () {
+                            setState(() {
+                              selected = [(false, 0), (false, 0), (false, 0)];
+                              selected[i] = (true, 20);
+                              widget.onClick(i);
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.home,
+                            color: Colors.white,
+                            size: 25,
+                          ),
+                        ),
+                      ],
                     ),
+                    if (i < 2) ...[
+                      const VerticalDivider(
+                        color: Colors.white,
+                        endIndent: 10,
+                        indent: 10,
+                        thickness: 1,
+                      )
+                    ]
                   ],
-                ),
-                if (i < 2) ...[
-                  const VerticalDivider(
-                    color: Colors.white,
-                    endIndent: 10,
-                    indent: 10,
-                    thickness: 1,
-                  )
-                ]
-              ],
-            ],
-          ),
-        ),
-      ),
-    );
+                ],
+              )),
+        ));
   }
 }
